a2fdf60320e57763290e3f7571cf5d2e7144aa08
Nieyuanbi <108716091+NieyYuanbi@users.noreply.github.com>
2024-07-23 15:45:03 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,43 +1,42 @@
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from train import FirstNet, My2DModel, IntegratedModel  # 从你的模型文件导入模型类
-from wrap.ADNI import AdniDataSet  # 从你的数据文件导入数据集类
+from train import FirstNet, My2DModel, IntegratedModel  
+from wrap.ADNI import AdniDataSet  
 from wrap.setting import parse_opts
 import csv
 from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score, roc_auc_score, roc_curve, auc
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 定义设备
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 sets = parse_opts()
 sets.gpu_id = [0]
 
-# 加载已训练的模型
+
 model_2d = My2DModel(f=8)
 model_3d = FirstNet(f=8)
 state_dict = torch.load('', map_location=torch.device('cpu'))
-# 移除 "module." 前缀
+
 new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
 model_3d.load_state_dict(new_state_dict)
 model_3d.to(device)
 
 model = IntegratedModel(model_2d, model_3d)
 model = torch.nn.DataParallel(model)
-model_weight_path = ""  # 模型权重文件路径
+model_weight_path = "" 
 model.load_state_dict(torch.load(model_weight_path, map_location=device))
 model.to(device)
 model.eval()
 
-# 加载测试数据集
+
 test_data_path = ''
 test_img_path = r''
 test_data = AdniDataSet(test_data_path, test_img_path, sets)
 test_loader = DataLoader(dataset=test_data, batch_size=16, shuffle=False)
-print("数据加载成功")
 
-# 迭代测试数据并进行预测
+
 predictions = []
 all_label = []
 probabilities =[]
@@ -78,23 +77,23 @@ with open(csvfile, 'w', newline='') as csvfile:
 accuracy = correct_predictions / len(predictions)
 print("Accuracy:", accuracy)
 
-# 将预测和标签转换为numpy数组
+
 predictions = np.array(predictions)
 all_label = np.array(all_label)
 probabilities = np.array(probabilities)
 
-# 计算混淆矩阵
+
 tn, fp, fn, tp = confusion_matrix(all_label, predictions).ravel()
 
-# 计算各项指标
+
 acc = accuracy_score(all_label, predictions)
-sen = recall_score(all_label, predictions)  # 敏感性
-spe = tn / (tn + fp)  # 特异性
-pre = precision_score(all_label, predictions)  # 精确度
-f1 = f1_score(all_label, predictions)  # F1分数
+sen = recall_score(all_label, predictions)  
+spe = tn / (tn + fp)  
+pre = precision_score(all_label, predictions)  
+f1 = f1_score(all_label, predictions)  
 AUC = roc_auc_score(all_label, probabilities)
 
-# 打印结果
+
 print("Accuracy:", acc)
 print("Sensitivity:", sen)
 print("Specificity:", spe)
@@ -102,11 +101,11 @@ print("Precision:", pre)
 print("F1 Score:", f1)
 print("Auc", AUC)
 
-# 计算ROC曲线
+
 fpr, tpr, thresholds = roc_curve(all_label, probabilities)
 roc_auc = auc(fpr, tpr)
 
-# 绘制ROC曲线
+
 plt.figure()
 plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
